0a3b9a8c18980e7dabc42719cbee7cfc3108dcba
LEESUKYONG <joara77@naver.com>
2024-11-01 00:58:47 +0900

    Initial commit

---
+++ Assets/Scripts/Fruit.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Fruit : MonoBehaviour
+{
+    public float timeAdd = 15;   // 1st 5 -> Edit version 10
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    { 
+        if (collision.tag == "Player" )
+        {
+            GameManager.Instance.AddTime(timeAdd);
+            GetComponent<Animator>().SetTrigger("Eaten");
+            Invoke("DestroyThis", 0.3f);
+
+
+        }
+    }
+
+    void DestroyThis()
+    {
+        Destroy(gameObject);
+    }
+
+
+}
